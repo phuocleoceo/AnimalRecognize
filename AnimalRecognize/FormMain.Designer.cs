e65a18e980141697a3f48851e3f8f4366cb2b4d3
phuocleoceo <ht10082001@gmail.com>
2021-02-14 13:25:17 +0700

    Done

---
+++ AnimalRecognize/FormMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace AnimalRecognize
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnRecognize = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.pbInput = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Result :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtInput.Location = new System.Drawing.Point(91, 13);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(421, 32);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOpenFile.Location = new System.Drawing.Point(525, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(48, 33);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResult.Location = new System.Drawing.Point(187, 359);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(305, 36);
+            this.txtResult.TabIndex = 3;
+            // 
+            // btnRecognize
+            // 
+            this.btnRecognize.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRecognize.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRecognize.Location = new System.Drawing.Point(244, 290);
+            this.btnRecognize.Name = "btnRecognize";
+            this.btnRecognize.Size = new System.Drawing.Size(109, 51);
+            this.btnRecognize.TabIndex = 4;
+            this.btnRecognize.Text = "Recognize";
+            this.btnRecognize.UseVisualStyleBackColor = false;
+            this.btnRecognize.Click += new System.EventHandler(this.btnRecognize_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(65, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Score :";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtScore.Location = new System.Drawing.Point(186, 415);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.ReadOnly = true;
+            this.txtScore.Size = new System.Drawing.Size(305, 36);
+            this.txtScore.TabIndex = 3;
+            // 
+            // pbInput
+            // 
+            this.pbInput.Location = new System.Drawing.Point(81, 67);
+            this.pbInput.Name = "pbInput";
+            this.pbInput.Size = new System.Drawing.Size(456, 206);
+            this.pbInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbInput.TabIndex = 5;
+            this.pbInput.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(20, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Input :";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 467);
+            this.Controls.Add(this.pbInput);
+            this.Controls.Add(this.btnRecognize);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMain";
+            this.Text = "Animal Recognize";
+            ((System.ComponentModel.ISupportInitialize)(this.pbInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnRecognize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.PictureBox pbInput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AnimalRecognize/FormMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AnimalRecognizeML.Model;
+
+namespace AnimalRecognize
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private string FilePath;
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Select Input File ";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = dlg.FileName;
+                txtInput.Text = FilePath;
+                pbInput.Image = Image.FromFile(FilePath);
+                btnRecognize.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Try Again !", "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRecognize_Click(object sender, EventArgs e)
+        {
+            var input = new ModelInput();
+            input.ImageSource = FilePath;
+            ModelOutput result = ConsumeModel.Predict(input);
+            txtResult.Text = result.Prediction;
+            foreach (var i in result.Score)
+            {
+                txtScore.Text += i.ToString() + " , ";
+            }
+        }
+    }
+}
